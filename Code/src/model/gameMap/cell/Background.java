4827d3c431def98a31f30062aa220026a9e908d2
Omar Foudane <ofoudane@iut.univ-paris8.fr>
2018-05-18 07:24:29 +0200

    William work merge

--- Code/src/model/gameMap/cell/Background.java
+++ Code/src/model/gameMap/cell/Background.java
@@ -1,5 +1,40 @@
-package model.gameMap.cell;
 
 public class Background {
 
+	private int value;
+	private int imageValue;
+	private boolean isWalkable;
+	
+	public Background() {
+		this.isWalkable=false;
+	}
+	
+	//Renvoie une IllegalArgumentException, si un des parametres est faux
+		public Background(int value,boolean isWalkable, int image) throws IllegalArgumentException { 
+			if(value < 0 || image < 0) {
+				throw new IllegalArgumentException("FALSE ARGUMENT ON BACKGROUND");
+			}else {
+				this.value=value;
+				this.isWalkable=isWalkable;
+				this.imageValue= image;
+			}
+		}	
+		public int getValue() {
+			return value;
+		}
+		
+		//retourne vrai si le fond est traversable, sinon renvoie faux.
+		public boolean getIsWalkable () {
+			return isWalkable;
+		}
+
+		//renvoie l’image qui renvoie la valeur de l’image
+		public int getImageValue() { 
+			return this.imageValue;
+		}
+	
+	public void setBackground(int imgVal) {
+		this.imageValue=imgVal;
+		/*this.isWalkable=BackgroundFactory.getWalkableImgVal(imgVal);*/ 
+	}
 }
